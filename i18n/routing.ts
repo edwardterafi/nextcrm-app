a70f7f53e1b563873a5a4a49f2587dba91f72982
edwardterafi <edward@terafi.me>
2026-07-28 14:17:54 +0800

    Update routing.ts

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,9 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ["en", "cz", "de", "uk"],
-
-  // Used when no locale matches
   defaultLocale: "en",
+  localePrefix: "as-needed",
 });
